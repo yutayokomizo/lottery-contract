ada438ce8bf3e99c1a90fb3f4805fc9fa72c2de2
yutayokomizo <yutayoko.prime@gmail.com>
2022-06-10 13:07:58 +0900

    Built lottery contract

---
+++ deploy.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const env = process.env;
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  env.MNEMONIC,
+  // remember to change this to your own phrase!
+  env.INFURA_RINKEBY
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
